2e6ee3c10fd26ef551b3fa37289bda31152f3141
stef <stef.m@hotmail.be>
2023-06-04 15:21:35 +0200

    Start en eind scene aangepast

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public Button FirstButton;
+
+    public Button PlayButton;
+    public Text PlayText;
+    public Button QuitButton;
+    public Text QuitText;
+    EventSystem system;
+
     void Start()
     {
         FirstButton.Select();
+        system = EventSystem.current;
     }
 
     public void Play()
@@ -20,5 +29,21 @@ public class MainMenu : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    void Update()
+    {
+        if (system.currentSelectedGameObject == PlayButton.gameObject)
+        {
+            PlayText.color = PlayButton.colors.selectedColor;
+            QuitText.color = QuitButton.colors.normalColor;
+        }
+
+
+        else
+        {
+            PlayText.color = PlayButton.colors.normalColor;
+            QuitText.color = QuitButton.colors.selectedColor;
+        }
     }
 }
